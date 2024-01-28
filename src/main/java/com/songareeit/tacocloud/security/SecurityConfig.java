9ac7cef3bfcbceb966d402d8906b0899919b6bb5
leedongyeop <ldy_1204@naver.com>
2024-01-28 16:10:23 +0900

    feat : 4장. JDBC 스토어 기반의 시큐리티

--- src/main/java/com/songareeit/tacocloud/security/SecurityConfig.java
+++ src/main/java/com/songareeit/tacocloud/security/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.songareeit.tacocloud.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -10,6 +12,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 @Configuration
@@ -30,20 +34,32 @@ public class SecurityConfig {
                 .build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(User.withUsername("user1")
+//                .password("{noop}password1")
+//                .roles("USER")
+//                .build());
+//
+//        manager.createUser(User.withUsername("user2")
+//                .password("{noop}password2")
+//                .roles("USER")
+//                .build());
+//
+//        return manager;
+//    }
 
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("user1")
-                .password("{noop}password1")
-                .roles("USER")
-                .build());
+    @Autowired
+    DataSource dataSource;
 
-        manager.createUser(User.withUsername("user2")
-                .password("{noop}password2")
-                .roles("USER")
-                .build());
-
-        return manager;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource);
     }
+
+
 }
