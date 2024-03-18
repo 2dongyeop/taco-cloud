77e1df1e8d6faaadedcbb385c88746c0c8923322
leedongyeop <ldy_1204@naver.com>
2024-03-18 23:07:41 +0900

    feat : ActiveMQ 구동을 위한 docker-compose 추가

--- src/main/java/com/songareeit/tacocloud/messaging/jms/provider/DestinationConfig.java
+++ src/main/java/com/songareeit/tacocloud/messaging/jms/provider/DestinationConfig.java
@@ -1,10 +1,14 @@
 package com.songareeit.tacocloud.messaging.jms.provider;
 
 import com.songareeit.tacocloud.domain.Order;
+import jakarta.jms.ConnectionFactory;
 import jakarta.jms.Destination;
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 import org.apache.activemq.artemis.jms.client.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 
 import java.util.HashMap;
@@ -15,11 +19,20 @@ import java.util.HashMap;
  * @description
  */
 @Configuration
+@EnableJms
 public class DestinationConfig {
+
+    @Value("${messaging.queue.order.name}")
+    private String orderQueueName;
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        return new ActiveMQConnectionFactory("tcp://localhost:61616");
+    }
 
     @Bean
     public Destination destination() {
-        return new ActiveMQQueue("tacocloud.order.queue");
+        return new ActiveMQQueue(orderQueueName);
     }
 
     @Bean
